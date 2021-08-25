a37e8716d126d7f07b7f13a9c45b17496ee90e43
Vitaliy <vitaliy.kostyukov@gmail.com>
2021-08-25 08:35:39 +0300

    change file HW10/treangle_sequence.py

--- HW10/treangle_sequence.py
+++ HW10/treangle_sequence.py
@@ -1,22 +1,28 @@
-def count_first(nums, n):
-    nums = 1
-    k = 0
-#    for i in range(1, n + 1):
-#        print(k)
-#        if i == k * (k + 1) // 2:
-#            k += 1
-    # return k
-    for i in range(n):
-        print(nums, end=' ', flush=True)
-        k = k + 1
-        if k == nums:
-            k = 0
-            nums = nums + 1
-    print()
+def count_first(n, k=1,):
+    if not n:
+        return
+    for _ in range(k):
+        print(k, end=' ')
+    count_first(n - 1, k + 1)
+    # nums = 1
+    # k = 0
+    #  for i in range(1, n + 1):
+    #      print(k)
+    #      if i == k * (k + 1) // 2:
+    #            k += 1
+    #  return k
+    # for i in range(n):
+    #     print(nums, end=' ', flush=True)
+    #     k = k + 1
+    #     if k == nums:
+    #        k = 0
+    #        nums = nums + 1
+    # print()
+    # for i in range(1, n + 1):
+    # return count_first(1, n)
 
 
-k1 = 1
-n1 = int(input('Введите число n: '))
-print(count_first(k1, n1))
-
+# n = int(input('Введите число n: '))
+# print(count_first(n))
+count_first(int(input('Введите число n: ')))
 
