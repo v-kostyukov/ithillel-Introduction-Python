dcd0a3b88b693a0416d2b9a4bf349ffc10ce8360
Vitaliy <vitaliy.kostyukov@gmail.com>
2021-09-03 12:43:26 +0300

    change line length to 80 characters in files HW11/sortm.py, mod1.py, mod2.py, use_package_mod1.py

--- HW11/use_package_mod1.py
+++ HW11/use_package_mod1.py
@@ -7,6 +7,6 @@ b = [5, 18, 34, 67, 44]
 
 words = input("Введите два слова: ")
 
-print("Уникальных чисел содержится одновременно как в первом списке, так и во втором: ", mod1.counts_unique_numbers(a, b))
-print("Кол-во чисел содержится одновременно как в первом списке, так и во втором: ", mod1.counts_numbers(a, b))
+print("Ун-ых чисел содерж. од-но в 1, 2: ", mod1.counts_unique_numbers(a, b))
+print("Кол-во чисел содержится од-но как в 1, 2: ", mod1.counts_numbers(a, b))
 print("=>", mod1.inverted_words(words))
